1902cdc716064414b3d90a2f6e2d6db6adb93b3f
adarsh tiwari <GooglerAdarsh@users.noreply.github.com>
2016-08-25 20:35:39 +0530

    Add files via upload

---
+++ Dijkstra_algo.cpp
@@ -0,0 +1,48 @@
+//Googler_adarsh_IIITA
+//work_when_0th_node_exist_else_u_have_to_do_some_changes
+#include<bits/stdc++.h>
+#define ll long long 
+#define pb(a) push_back(a);
+using namespace std;
+int arr[888][67];
+queue<ll> q;
+ll n,dst[99999],s;
+void dks(ll src){
+	q.push(src);
+	while(!q.empty()){
+		s=q.front();
+		q.pop();
+		for(ll i=0;i<=n;i++)
+		{
+			if(arr[s][i])
+			{
+				if(dst[i]>(dst[s]+arr[s][i]))
+				{
+					dst[i]=dst[s]+arr[s][i];
+                                          q.push(i);
+				} 
+			}
+		}
+    }
+ for(ll i=0;i<=n;i++)
+cout<<dst[i]<<" ";
+}
+int main(){
+	ll w,e,src,u,v;
+	cout<<"Enter no_node_&_edge"<<endl;
+	cin>>n>>e;
+	for(ll i=0;i<e;i++)
+	{
+		cin>>u>>v>>w;
+		arr[u][v]=w;
+		arr[v][u]=w;
+
+	}
+	for(ll i=0;i<=n;i++)
+		dst[i]=INT_MAX;
+	cout<<"Enter source"<<endl;
+	cin>>src;
+	dst[src]=0;
+	dks(src);
+	return 0;
+}
