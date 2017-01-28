8104f9bbec7de0de2ddcec7b938528b4850d5cf4
adarsh tiwari <GooglerAdarsh@users.noreply.github.com>
2017-01-28 05:08:08 -0800

    Update BST.cpp

--- BST.cpp
+++ BST.cpp
@@ -6,6 +6,7 @@ struct node *left;
 struct node *right;
 };
 queue<int>st;
+map<int,bool>mp;
 struct node *new_node(int da){
     struct node *node1;
     node1=(struct node *)malloc(sizeof(struct node));
@@ -57,6 +58,33 @@ void print_leaf(struct node *temp){
            st.pop();
         }
 }
+int  print_hight(struct node *temp){
+    if(temp==NULL)
+        return 0;
+int l=print_hight(temp->left);
+int r=print_hight(temp->right);
+if(l>r)
+    return (l+1);
+else
+    return (r+1);
+}
+void printbfs(struct node *temp,int h){
+    if(temp==NULL)
+    return ;
+    if(h==1){
+          cout<<temp->data<<" ";
+    }
+    else{
+             printbfs(temp->left,h-1);
+             printbfs(temp->right,h-1);
+    }
+}
+void print_BFS(struct node *temp,int h){
+    for(int i=1;i<=h;i++)
+    {
+        printbfs(temp,i);
+    }
+}
 int main(){
 struct node *root=NULL;
     while(1)
@@ -103,9 +131,16 @@ struct node *root=NULL;
     print_POSTORDER(root);
      cout<<endl<<"Size of tree"<<endl;
     cout<<(sizeoftree(root->left)+1+sizeoftree(root->right))<<endl;
-      cout<<endl<<"DFS trevesal"<<endl;
+      cout<<endl<<"DFS traversal"<<endl;
       print_DFS(root);
       cout<<endl<<"printing Leaf node"<<endl;
         print_leaf(root);
+          cout<<endl<<"printing Hight of tree"<<endl;
+          cout<<print_hight(root)<<endl;
+          cout<<endl<<"BFS traversal"<<endl;
+          print_BFS(root,print_hight(root));
+          cout<<endl<<"Left view"<<endl;
+          print_BFS(root,print_hight(root));
+
 return 0;
 }
